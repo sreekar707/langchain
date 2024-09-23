d442b476dc4cef18b7c77e1997b591a80c2498e5
sreekar707 <sreekarb2005@gmail.com>
2024-09-24 02:01:42 +0530

    ragg

---
+++ api/app.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+from langchain.prompts import ChatPromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langserve import add_routes
+import uvicorn
+import os
+from langchain_community.llms import Ollama
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+app = FastAPI(
+    title="Langchain Server",
+    version="1.0",
+    description="A simple API Server"
+)
+
+add_routes(
+    app,
+    Ollama(),
+    path="/openai"
+)
+
+#  model = Ollama()
+# ollama llama2
+llm = Ollama(model="llama2")
+
+prompt1 = ChatPromptTemplate.from_template("Write me an essay about {topic} with 100 words")
+prompt2 = ChatPromptTemplate.from_template("Write me a poem about {topic} for a 5-year-old child with 100 words")
+
+add_routes(
+    app,
+    prompt1 | llm,
+    path="/essay"
+)
+
+add_routes(
+    app,
+    prompt2 | llm,
+    path="/poem"
+)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
